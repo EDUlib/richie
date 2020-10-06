6fec6c3d7ab973b4ad1009b66faf3f8586803949
Truong David <david.truong@edulib.org>
2020-10-06 13:55:27 -0400

    🚀(edulib) fix after rebasing with v2.15

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -267,37 +267,6 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
 
     # LMS
     LMS_BACKENDS = [
-        {
-            # We configure default values that work with the test configuration of
-            # github.com/openfun/openedx-docker.
-            "BACKEND": values.Value(
-                "richie.apps.courses.lms.base.BaseLMSBackend",
-                environ_name="EDX_BACKEND",
-                environ_prefix=None,
-            ),
-            "JS_BACKEND": values.Value(
-                "base",
-                environ_name="EDX_JS_BACKEND",
-                environ_prefix=None,
-            ),
-            "COURSE_REGEX": values.Value(
-                r".*", environ_name="EDX_COURSE_REGEX", environ_prefix=None
-            ),
-            "SELECTOR_REGEX": values.Value(
-                r"^(?P<course_id>.*)$",
-                environ_name="EDX_SELECTOR_REGEX",
-                environ_prefix=None,
-            ),
-            "JS_SELECTOR_REGEX": values.Value(
-                r".*", environ_name="EDX_JS_SELECTOR_REGEX", environ_prefix=None
-            ),
-            "JS_COURSE_REGEX": values.Value(
-                r"^(?<course_id>.*)$",
-                environ_name="EDX_JS_COURSE_REGEX",
-                environ_prefix=None,
-            ),
-            "BASE_URL": values.Value(environ_name="EDX_BASE_URL", environ_prefix=None),
-        }
     ]
 
     # Internationalization
